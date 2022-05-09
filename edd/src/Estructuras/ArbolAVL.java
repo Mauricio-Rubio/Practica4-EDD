8840ca5175bb91beb3a6d23bb1b2c3a7cb92b636
Mauricio Rubio Haro <mauricio.rubio@ciencias.unam.mx>
2022-05-09 14:51:45 -0500

    Clase vertice para AVL
    Revisar su funcionamiento

---
+++ edd/src/Estructuras/ArbolAVL.java
@@ -0,0 +1,42 @@
+package edd.src.Estructuras;
+
+public class ArbolAVL<T extends Comparable<T>> extends ArbolBinarioBusqueda {
+
+  protected class VerticeAVL extends Vertice {
+
+    public int altura;
+
+    public VerticeAVL(T elemento) {
+      super(elemento);
+      altura = 0;
+    }
+
+    /**
+     * Regresa una representación en cadena del vértice rojinegro.
+     *
+     * @return una representación en cadena del vértice rojinegro.
+     */
+    public String toString() {
+      return super.toString()+" altura: "+this.altura;
+    }
+
+    /**
+     * Compara el vértice con otro objeto. La comparación es
+     * <em>recursiva</em>.
+     *
+     * @param o el objeto con el cual se comparará el vértice.
+     * @return <code>true</code> si el objeto es instancia de la clase
+     *         {@link VerticeAVL}, su elemento es igual al elemento de
+     *         éste vértice, los descendientes de ambos son recursivamente
+     *         iguales, y los colores son iguales; <code>false</code> en
+     *         otro caso.
+     */
+    @Override
+    public boolean equals(Object o) {
+      if (o == null || getClass() != o.getClass()) return false;
+      @SuppressWarnings("unchecked")
+      VerticeAVL vertice = (VerticeAVL) o;
+      return altura == vertice.altura && super.equals(o);
+    }
+  }
+}
