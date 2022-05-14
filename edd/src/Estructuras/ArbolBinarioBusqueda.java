05e0ad4c66cbb2dc052bf8ffda7cf41fdeefd143
Mauricio Rubio Haro <mauricio.rubio@ciencias.unam.mx>
2022-05-13 20:37:04 -0500

    Correccion del delete

--- edd/src/Estructuras/ArbolBinarioBusqueda.java
+++ edd/src/Estructuras/ArbolBinarioBusqueda.java
@@ -4,7 +4,8 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class ArbolBinarioBusqueda<T extends Comparable<T>> extends ArbolBinario<T> {
+public class ArbolBinarioBusqueda<T extends Comparable<T>>
+  extends ArbolBinario<T> {
 
   private class Iterador implements Iterator<T> {
 
@@ -41,31 +42,29 @@ public class ArbolBinarioBusqueda<T extends Comparable<T>> extends ArbolBinario<
     }
   }
 
- private boolean esHijoIzq = false;
+  private boolean esHijoIzq = false;
 
- public Vertice verticeReemplazo(Vertice vertice) {
-  Vertice reemplazarPadre = vertice;
-  Vertice reemplazo = vertice;
-  Vertice aux = vertice.derecho;
-  while (aux != null) {
-    reemplazarPadre = reemplazo;
-    reemplazo = aux;
-    aux = aux.izquierdo;
+  public Vertice verticeReemplazo(Vertice vertice) {
+    Vertice reemplazarPadre = vertice;
+    Vertice reemplazo = vertice;
+    Vertice aux = vertice.derecho;
+    while (aux != null) {
+      reemplazarPadre = reemplazo;
+      reemplazo = aux;
+      aux = aux.izquierdo;
+    }
+    if (!reemplazo.equals(vertice.derecho)) {
+      reemplazarPadre.izquierdo = reemplazo.derecho;
+      reemplazo.derecho = vertice.derecho;
+    }
+    //System.out.println("El padre reemplazo es "+ reemplazo.padre);
+    //System.out.println("El reemplazo es "+ reemplazo);
+    //System.out.println("Es hijIzq"+ esHijoIzq);
+    return reemplazo;
   }
-  if (!reemplazo.equals(vertice.derecho)) {
-    reemplazarPadre.izquierdo = reemplazo.derecho;
-    reemplazo.derecho = vertice.derecho;
-  }
-  System.out.println("El padre reemplazo es "+ reemplazo.padre);
-  System.out.println("El reemplazo es "+ reemplazo);
-  System.out.println("Es hijIzq"+ esHijoIzq);
-  return reemplazo;
-}
-
-
-
 
   public boolean delete(T object) {
+    //System.out.println("Iniciando estaa mmamada : " + object);
     Vertice vertice = this.search(this.raiz, object);
     if (vertice == null) {
       System.out.println("No se ha encontrado");
@@ -77,61 +76,93 @@ public class ArbolBinarioBusqueda<T extends Comparable<T>> extends ArbolBinario<
       Vertice reemplazo = verticeReemplazo(aux);
       if (aux.equals(this.raiz)) {
         this.raiz = reemplazo;
-      } else if (padre.izquierdo.get().compareTo(aux.elemento)==0) {
+      } else if (esHijoIzq) {
         padre.izquierdo = reemplazo;
-        reemplazo.padre=padre;
       } else {
         padre.derecho = reemplazo;
-        reemplazo.padre=padre;
       }
       reemplazo.izquierdo = aux.izquierdo;
-      reemplazo.izquierdo.padre = reemplazo;
-      System.out.println("REMp izq " + reemplazo.izquierdo);
-      System.out.println("REMp der" + reemplazo.derecho);
-      System.out.println("REMp izq padre" + reemplazo.izquierdo.padre);
-     // System.out.println("REMp der padre " + reemplazo.derecho.padre);
       return true;
     }
+    Vertice parent = null;
+    Vertice curr = this.raiz;
+    while (curr != null && curr.elemento != object) {
+      // update the parent to the current node
+      parent = curr;
+
+      // if the given key is less than the current node, go to the left subtree;
+      // otherwise, go to the right subtree
+      //if (object < curr.elemento) {
+      if (curr.elemento.compareTo(object) > 0) {
+        curr = curr.izquierdo;
+      } else {
+        curr = curr.derecho;
+      }
+    }
+    if (curr.izquierdo == null && curr.derecho == null) {
+      // if the node to be deleted is not a root node, then set its
+      // parent left/right child to null
+      if (curr != this.raiz) {
+        if (parent.izquierdo == curr) {
+          parent.izquierdo = null;
+        } else {
+          parent.derecho = null;
+        }
+      }
+      // if the tree has only a root node, set it to null
+      else {
+        this.raiz = null;
+      }
+    } else {
+      // choose a child node
+      Vertice child = (curr.izquierdo != null) ? curr.izquierdo : curr.derecho;
+
+      // if the node to be deleted is not a root node, set its parent
+      // to its child
+      if (curr != this.raiz) {
+        if (curr == parent.izquierdo) {
+          parent.izquierdo = child;
+        } else {
+          parent.derecho = child;
+        }
+      }
+      // if the node to be deleted is a root node, then set the root to the child
+      else {
+        this.raiz = child;
+      }
+    }
+
+    /*
     if (vertice.izquierdo == null && vertice.derecho == null) {
-      System.out.println("PADRE "+ vertice.padre);
       if (vertice.equals(this.raiz)) {
         this.raiz = null;
         return true;
       }
       if (vertice.padre.get().compareTo(vertice.elemento) < 0) {
         vertice.padre.derecho = null;
-        vertice=null;
         return true;
       } else {
         vertice.padre.izquierdo = null;
-        vertice=null;
       }
       return true;
     }
     if (vertice.izquierdo == null || vertice.derecho != null) {
       if (vertice.padre.get().compareTo(vertice.elemento) < 0) {
         vertice.padre.derecho = vertice.derecho;
-        vertice.derecho.padre=vertice.padre;
       } else {
         vertice.padre.izquierdo = vertice.derecho;
-        vertice.derecho.padre=vertice.padre;
-        //vertice=null;
       }
       return true;
     }
     if (vertice.izquierdo != null || vertice.derecho == null) {
       if (vertice.padre.get().compareTo(vertice.elemento) < 0) {
-        System.out.println(vertice.padre.get());
         vertice.padre.derecho = vertice.izquierdo;
-        System.out.println("Ojo");
-        System.out.println(vertice.padre.get());
       } else {
         vertice.padre.izquierdo = vertice.izquierdo;
-        System.out.println("Ojo2");
       }
       //vertice = vertice.izquierdo;
       return true;
-    }
+    }*/
 
     return true;
   }
@@ -183,16 +214,14 @@ public class ArbolBinarioBusqueda<T extends Comparable<T>> extends ArbolBinario<
     return null;
   }
 
-   
   /**
    * Metodo que regresa el recorrido BFS del arbol
    * @author Alcantara Estrada Kevin Isaac
    * @author Rubio Haro Mauricio
    * @param arbolE Arbol binario
    */
- 
-  protected Cola<T> modBFS(ArbolBinario arbolE) {
 
+  protected Cola<T> modBFS(ArbolBinario arbolE) {
     //Si es vacio no procedemos
     if (arbolE.isEmpty()) {
       System.out.println("No puedo proceder con un arbol vacio");
@@ -200,7 +229,7 @@ public class ArbolBinarioBusqueda<T extends Comparable<T>> extends ArbolBinario<
     }
     //Colas para llevar a cabo el recorrido
     Cola<Vertice> a = new Cola<Vertice>();
-     Cola<T> colaBFS = new Cola<T>();
+    Cola<T> colaBFS = new Cola<T>();
     //Anadimos vertice inicial
     a.push(arbolE.raiz);
     //Mientras la cola no sea vacia, proseguimos con el algoritmo
@@ -215,7 +244,7 @@ public class ArbolBinarioBusqueda<T extends Comparable<T>> extends ArbolBinario<
         a.push(b.derecho);
       }
     }
-   return colaBFS;
+    return colaBFS;
   }
 
   /**
@@ -223,11 +252,10 @@ public class ArbolBinarioBusqueda<T extends Comparable<T>> extends ArbolBinario<
    * @author Alcantara Estrada Kevin
    * @author Rubio Haro Mauricio
    * @param verti Vertice inicial (usualmente la raiz)
-   * @return Verti 
+   * @return Verti
    */
   protected Vertice ultimoIzquierdo(Vertice verti) {
-    while(verti.hayIzquierdo()){
-    
+    while (verti.hayIzquierdo()) {
       verti = verti.izquierdo;
     }
     return verti;
@@ -238,13 +266,11 @@ public class ArbolBinarioBusqueda<T extends Comparable<T>> extends ArbolBinario<
    * @author Alcantara Estrada Kevin
    * @author Rubio Haro Mauricio
    * @param verti Vertice inicial (usualmente la raiz)
-   * @return Verti 
+   * @return Verti
    */
   protected Vertice ultimoDerecho(Vertice verti) {
-    
     //Mientras el vertice tenga vertice derecho, recorremos el arbol
-      while(verti.hayDerecho()){
-    
+    while (verti.hayDerecho()) {
       verti = verti.derecho;
     }
     return verti;
@@ -274,10 +300,6 @@ public class ArbolBinarioBusqueda<T extends Comparable<T>> extends ArbolBinario<
     return null;
   }
 
-
- 
-
- 
   /**
    * Metodo para ordenar una lista haciendo uso del metodo quicksort
    * @author Alcantara Estrada Kevin Isaac
@@ -339,37 +361,30 @@ public class ArbolBinarioBusqueda<T extends Comparable<T>> extends ArbolBinario<
 
     //return lista;
   }
-  
 
-/**
- * Metodo para construir un arbol de acuerdo a una lista desordenada
- * @author Alcantara Estrada Kevin
- * @author Rubio Haro Mauricio
- * @param lista Lista desordenada
- */
+  /**
+   * Metodo para construir un arbol de acuerdo a una lista desordenada
+   * @author Alcantara Estrada Kevin
+   * @author Rubio Haro Mauricio
+   * @param lista Lista desordenada
+   */
   public void buildUnsorted(Lista<T> lista) {
     ArbolBinarioBusqueda<T> arbolinio = new ArbolBinarioBusqueda<T>();
     //Si la lista es vacia
-    if(lista.isEmpty()){
+    if (lista.isEmpty()) {
       System.out.println("No puedo proceder con una lista vacia");
       return;
     }
     //Ordenamos la lista con el algoritmo quicksort y como la lista es desordenada, nos toma O(nlog(n))
     ordenarLista(lista, 0, lista.size() - 1);
-    System.out.println(lista.toString());
+    //System.out.println("Mostrando lista"+lista.toString());
     /*
     T elem = lista.elemIndice(lista.size() / 2);
     arbolinio.add(elem);
     lista.delete(elem);*/
     buildSorted(lista);
-     
-    }
-   
+  }
 
-  
-
-
-  
   /**
    * Metodo para insertar un elemento en el arbol
    * @author Alcantara Estrada Kevin Isaac
@@ -377,35 +392,32 @@ public class ArbolBinarioBusqueda<T extends Comparable<T>> extends ArbolBinario<
    * @param elemento Elemento a insertar
    */
 
-  public void insert(Vertice verti, T elemento){
+  public void insert(Vertice verti, T elemento) {
     /*if(verti==null){
       Vertice nuevo = nuevoVertice(elemento);
       this.add(elemento);
     }*/
-        if(verti.get().compareTo(elemento)>0){
-            if(!verti.hayIzquierdo()){
-                Vertice nuevo = nuevoVertice(elemento);
-                verti.izquierdo=nuevo;
-                nuevo.padre=verti;
-            }else{
-                insert(verti.izquierdo,elemento);
-            }
-        }
-
-        if(verti.get().compareTo(elemento)<0){
-            if(!verti.hayDerecho()){
-                Vertice nuevo = nuevoVertice(elemento);
-                verti.derecho=nuevo;
-                nuevo.padre=verti;
-            }else{
-                insert(verti.derecho,elemento);
-            }
-        }
-
+    if (verti.get().compareTo(elemento) > 0) {
+      if (!verti.hayIzquierdo()) {
+        Vertice nuevo = nuevoVertice(elemento);
+        verti.izquierdo = nuevo;
+        nuevo.padre = verti;
+      } else {
+        insert(verti.izquierdo, elemento);
       }
+    }
 
+    if (verti.get().compareTo(elemento) < 0) {
+      if (!verti.hayDerecho()) {
+        Vertice nuevo = nuevoVertice(elemento);
+        verti.derecho = nuevo;
+        nuevo.padre = verti;
+      } else {
+        insert(verti.derecho, elemento);
+      }
+    }
+  }
 
-       
   /**
    * Metodo para crear el arbol conn una lista ordenada
    * @author Alcantara Estrada Kevin Isaac
@@ -427,10 +439,10 @@ public class ArbolBinarioBusqueda<T extends Comparable<T>> extends ArbolBinario<
     } else {
       mitad = lista.size() / 2;
     }
-    System.out.println("Lista-->" + lista);
+    //System.out.println("Lista-->" + lista);
     //System.out.println("Mitad "+lista.size()/2);
     this.raiz = new Vertice(lista.eliminarIndice(mitad));
-    System.out.println("Lista-->" + lista);
+    //System.out.println("Lista-->" + lista);
     Lista<Integer> listaIzq = new Lista<>();
     IteradorLista<T> iterador = lista.iteradorLista();
     /**
@@ -466,93 +478,87 @@ public class ArbolBinarioBusqueda<T extends Comparable<T>> extends ArbolBinario<
     return;
   }
 
+  /**
+   * Metodo que devuelve un toString del arbol con el recorrido in orden DFS
+   * @author Alcantara Estrada Kevin Isaac
+   * @author Rubio Haro Mauricio
+   * @
+   */
+  Lista<T> colaDFS = new Lista<T>();
 
-
-    /**
-     * Metodo que devuelve un toString del arbol con el recorrido in orden DFS
-     * @author Alcantara Estrada Kevin Isaac
-     * @author Rubio Haro Mauricio
-     * @
-     */
-    Lista<T> colaDFS = new Lista<T>();
-   // Override
-    public String ptoString(){
-      //LLevamos el algoritmo hasta tener al final de la lista al elemnto mas grande de todo el arbol
-      while((!colaDFS.contains(ultimoDerecho(this.raiz).get()))){
-       // System.out.println("Ultimo Der" +ultimoDerecho(this.raiz).get());
+  // Override
+  public String ptoString() {
+    //LLevamos el algoritmo hasta tener al final de la lista al elemnto mas grande de todo el arbol
+    while ((!colaDFS.contains(ultimoDerecho(this.raiz).get()))) {
+      // System.out.println("Ultimo Der" +ultimoDerecho(this.raiz).get());
       inOrderDFS(raiz.izquierdo, raiz, raiz.derecho);
-      }
-       String s= colaDFS.toString();
-       return s;
-
     }
-
-    /**
-     * Metodo que recorre el arbol en DFS inOrder y devuelve la lista con los vertices
-     * @author Alcantara Estrada Kevin
-     * @author Rubio Haro Mauricio
-     * @param izq Vertice izquierdo
-     * @param cen Vertice del centro
-     * @param der Vertice de la derecha
-     */
-    private void inOrderDFS(Vertice izq, Vertice cen, Vertice der){
-     
-      //Mientras el elemento que checamos no este en la lista
-      if(!colaDFS.contains(cen.get())){
-        //mientras haya vertice izquierdo y este no este ya en la lista
-      if(cen.hayIzquierdo()&&!colaDFS.contains(izq.get())){
-        //seguimos recorriendo hacia la izquierda
-        cen= cen.izquierdo;
-        inOrderDFS(cen.izquierdo,cen,cen.derecho);
-      }else{
-        //agregamos el vertice sobre el que estamos a la lista
-      T elemento = cen.get();
-        colaDFS.add(elemento);
-       
-        //si hay vertices derechos los recorremos con este algoritmo
-        if(cen.hayDerecho()){
-          cen=cen.derecho;
-          inOrderDFS(cen.izquierdo,cen,cen.derecho);
-          }
-         
-        }
-      }else{
-        //De nuevo revisamos si hay derecho para seguir el algoritmo por ahi
-        if(cen.hayDerecho()){ 
-          cen=cen.derecho;
-          inOrderDFS(cen.izquierdo,cen,cen.derecho);
-          }
-      }
-      }
-    
-
-      /**
- * Metodo que toma un arbol binario y lo transforma en un Arbol Binario de Busqueda balanceado y devuelve este
- * @param arb Arbol
- * @return ArbolBinarioBusqueda
- */
-public ArbolBinarioBusqueda convertBST(ArbolBinario arb){
-  
- Cola<T> colaBFS = modBFS(arb);
- // System.out.println("COLA" +colaBFS.toString());
- Lista<T> lista = new Lista<T>();
- Lista<T> mitad1 = new Lista<T>();
-  Lista<T> mitad2 = new Lista<T>();
-
-  //nuevo.add(colaBFS.pop());
-  while(!colaBFS.isEmpty()){
-    lista.add(colaBFS.pop());
+    String s = colaDFS.toString();
+    return s;
   }
-  ArbolBinarioBusqueda nuevo = new ArbolBinarioBusqueda(lista,true);
+
+  /**
+   * Metodo que recorre el arbol en DFS inOrder y devuelve la lista con los vertices
+   * @author Alcantara Estrada Kevin
+   * @author Rubio Haro Mauricio
+   * @param izq Vertice izquierdo
+   * @param cen Vertice del centro
+   * @param der Vertice de la derecha
+   */
+  private void inOrderDFS(Vertice izq, Vertice cen, Vertice der) {
+    //Mientras el elemento que checamos no este en la lista
+    if (!colaDFS.contains(cen.get())) {
+      //mientras haya vertice izquierdo y este no este ya en la lista
+      if (cen.hayIzquierdo() && !colaDFS.contains(izq.get())) {
+        //seguimos recorriendo hacia la izquierda
+        cen = cen.izquierdo;
+        inOrderDFS(cen.izquierdo, cen, cen.derecho);
+      } else {
+        //agregamos el vertice sobre el que estamos a la lista
+        T elemento = cen.get();
+        colaDFS.add(elemento);
+
+        //si hay vertices derechos los recorremos con este algoritmo
+        if (cen.hayDerecho()) {
+          cen = cen.derecho;
+          inOrderDFS(cen.izquierdo, cen, cen.derecho);
+        }
+      }
+    } else {
+      //De nuevo revisamos si hay derecho para seguir el algoritmo por ahi
+      if (cen.hayDerecho()) {
+        cen = cen.derecho;
+        inOrderDFS(cen.izquierdo, cen, cen.derecho);
+      }
+    }
+  }
+
+  /**
+   * Metodo que toma un arbol binario y lo transforma en un Arbol Binario de Busqueda balanceado y devuelve este
+   * @param arb Arbol
+   * @return ArbolBinarioBusqueda
+   */
+  public ArbolBinarioBusqueda convertBST(ArbolBinario arb) {
+    Cola<T> colaBFS = modBFS(arb);
+    // System.out.println("COLA" +colaBFS.toString());
+    Lista<T> lista = new Lista<T>();
+    Lista<T> mitad1 = new Lista<T>();
+    Lista<T> mitad2 = new Lista<T>();
+
+    //nuevo.add(colaBFS.pop());
+    while (!colaBFS.isEmpty()) {
+      lista.add(colaBFS.pop());
+    }
+    ArbolBinarioBusqueda nuevo = new ArbolBinarioBusqueda(lista, true);
 
     return nuevo;
-}
+  }
 
-/**
- * Metodo que crear al Arbol BInario
- * @param lista Lista sobre la cual construirlo
- * @param isSorted para decir si esta ordenada la lista o no
- */
+  /**
+   * Metodo que crear al Arbol BInario
+   * @param lista Lista sobre la cual construirlo
+   * @param isSorted para decir si esta ordenada la lista o no
+   */
   public ArbolBinarioBusqueda(Lista<T> lista, boolean isSorted) {
     if (isSorted) {
       buildSorted(lista);
@@ -562,10 +568,9 @@ public ArbolBinarioBusqueda convertBST(ArbolBinario arb){
     }
   }
 
-
-/**
- * Metodo que constructor sin parametros
- */
+  /**
+   * Metodo que constructor sin parametros
+   */
   public ArbolBinarioBusqueda() {}
 
   /**
@@ -578,4 +583,4 @@ public ArbolBinarioBusqueda convertBST(ArbolBinario arb){
     return new Iterador();
   }
 }
- // }
+// }
--- edd/src/Estructuras/Practica4.java
+++ edd/src/Estructuras/Practica4.java
@@ -1,61 +1,86 @@
 package edd.src.Estructuras;
 
 public class Practica4 {
-    public static void main(String[] args) {
-      ArbolAVL miArbolito = new ArbolAVL();
 
-      miArbolito.add(10);
-      System.out.println("Mi arbolito \n" + miArbolito);
-      miArbolito.add(8);
-      System.out.println("Mi arbolito \n" + miArbolito);
-      miArbolito.add(12);
-      System.out.println("Mi arbolito \n" + miArbolito);
-      miArbolito.add(7);
-      System.out.println("Mi arbolito \n" + miArbolito);
-     // miArbolito.add(13);
-      miArbolito.add(9);
-      System.out.println("Mi arbolito \n" + miArbolito);
+  public static void main(String[] args) {
+    ArbolBinarioBusqueda miArbolito;
+    Lista miLista = new Lista();
+    for (int i = 0; i < 10; i++) {
+       miLista.add(i); 
+    }
+    /**
+     * Pruebas para delete
+     */
+    miArbolito = new ArbolBinarioBusqueda(miLista, true);
+    System.out.println("Este es mi arbolito \n"+miArbolito);
+    System.out.println("Delete 4");
+    miArbolito.delete(4);
+    System.out.println("Este es mi arbolito \n"+miArbolito);
+    System.out.println("Delete 2");
+    miArbolito.delete(2);
+    System.out.println("Este es mi arbolito \n"+miArbolito);
+    System.out.println("Delete 5");
+    miArbolito.delete(5);
+    System.out.println("Este es mi arbolito \n"+miArbolito);
+    
+    miArbolito.insert(miArbolito.raiz,2);
+    System.out.println("Este es mi arbolito \n"+miArbolito);
+    
+    System.out.println("Delete 3");
+    miArbolito.delete(3);
+    System.out.println("Este es mi arbolito \n"+miArbolito);
+    /*miArbolito.add(10);
+    System.out.println("Mi arbolito \n" + miArbolito);
+    miArbolito.add(8);
+    System.out.println("Mi arbolito \n" + miArbolito);
+    miArbolito.add(12);
+    System.out.println("Mi arbolito \n" + miArbolito);
+    miArbolito.add(7);
+    System.out.println("Mi arbolito \n" + miArbolito);
+    // miArbolito.add(13);
+    miArbolito.add(9);
+    System.out.println("Mi arbolito \n" + miArbolito);
 
-     //System.out.println( miArbolito.rotarDerecha(miArbolito.search(miArbolito.raiz,10)).toString());
-      System.out.println("Mi arbolito \n" + miArbolito);
-      miArbolito.insert(miArbolito.raiz,47);
-      System.out.println("Mi arbolito \n" + miArbolito);
-     miArbolito.add(44);
-      //miArbolito.rotarIzquierda(miArbolito.search(miArbolito.raiz,44));
-      System.out.println("Mi arbolito \n" + miArbolito);
-     miArbolito.add(48);
-      System.out.println("Mi arbolito \n" + miArbolito);
-      miArbolito.add(120);
-      System.out.println("Mi arbolito \n" + miArbolito);
-      miArbolito.add(110);
-      System.out.println("Mi arbolito \n" + miArbolito);
-      System.out.println("Mi arbolito \n"+miArbolito.modBFS(miArbolito));
-      miArbolito.add(105);
-      System.out.println("Mi arbolito \n" + miArbolito);
-      miArbolito.add(108);
-      System.out.println("Mi arbolito \n" + miArbolito);
-      miArbolito.add(109);
-      System.out.println("Mi arbolito \n" + miArbolito);
-      System.out.println("Mi arbolito \n"+miArbolito.modBFS(miArbolito));
-     miArbolito.add(-105);
-      System.out.println("Mi arbolito \n" + miArbolito);
-      miArbolito.add(13);
-      System.out.println("Mi arbolito \n" + miArbolito);
-      System.out.println("Mi arbolito \n"+miArbolito.modBFS(miArbolito));
-      miArbolito.add(19);
-      System.out.println("Mi arbolito \n" + miArbolito);
-      miArbolito.add(-23);
-      System.out.println("Mi arbolito \n" + miArbolito);
-      miArbolito.add(-3);
-      System.out.println("Mi arbolito \n" + miArbolito);
-      System.out.println("Mi arbolito \n"+miArbolito.modBFS(miArbolito));
-      miArbolito.add(-15);
-      System.out.println("Mi arbolito \n" + miArbolito);
-      miArbolito.add(-19);
-      System.out.println("Mi arbolito \n" + miArbolito);
-      miArbolito.add(5);
-      System.out.println("Mi arbolito \n" + miArbolito);
-     /* miArbolito.delete(-105);
+    //System.out.println( miArbolito.rotarDerecha(miArbolito.search(miArbolito.raiz,10)).toString());
+    System.out.println("Mi arbolito \n" + miArbolito);
+    miArbolito.insert(miArbolito.raiz, 47);
+    System.out.println("Mi arbolito \n" + miArbolito);
+    miArbolito.add(44);
+    //miArbolito.rotarIzquierda(miArbolito.search(miArbolito.raiz,44));
+    System.out.println("Mi arbolito \n" + miArbolito);
+    miArbolito.add(48);
+    System.out.println("Mi arbolito \n" + miArbolito);
+    miArbolito.add(120);
+    System.out.println("Mi arbolito \n" + miArbolito);
+    miArbolito.add(110);
+    System.out.println("Mi arbolito \n" + miArbolito);
+    System.out.println("Mi arbolito \n" + miArbolito.modBFS(miArbolito));
+    miArbolito.add(105);
+    System.out.println("Mi arbolito \n" + miArbolito);
+    miArbolito.add(108);
+    System.out.println("Mi arbolito \n" + miArbolito);
+    miArbolito.add(109);
+    System.out.println("Mi arbolito \n" + miArbolito);
+    System.out.println("Mi arbolito \n" + miArbolito.modBFS(miArbolito));
+    miArbolito.add(-105);
+    System.out.println("Mi arbolito \n" + miArbolito);
+    miArbolito.add(13);
+    System.out.println("Mi arbolito \n" + miArbolito);
+    System.out.println("Mi arbolito \n" + miArbolito.modBFS(miArbolito));
+    miArbolito.add(19);
+    System.out.println("Mi arbolito \n" + miArbolito);
+    miArbolito.add(-23);
+    System.out.println("Mi arbolito \n" + miArbolito);
+    miArbolito.add(-3);
+    System.out.println("Mi arbolito \n" + miArbolito);
+    System.out.println("Mi arbolito \n" + miArbolito.modBFS(miArbolito));
+    miArbolito.add(-15);
+    System.out.println("Mi arbolito \n" + miArbolito);
+    miArbolito.add(-19);
+    System.out.println("Mi arbolito \n" + miArbolito);
+    miArbolito.add(5);
+    System.out.println("Mi arbolito \n" + miArbolito);*/
+    /* miArbolito.delete(-105);
       System.out.println("Mi arbolito \n" + miArbolito);
       miArbolito.delete(108);
       System.out.println("Mi arbolito \n" + miArbolito);
@@ -108,14 +133,13 @@ public class Practica4 {
       miArbolito.add(17);
       System.out.println("Mi arbolito \n"+miArbolito);
       miArbolito.add(11);*/
-     /* System.out.println("Mi arbolito \n"+miArbolito);/
+    /* System.out.println("Mi arbolito \n"+miArbolito);/
      miArbolito.rotarD(miArbolito.search(miArbolito.raiz,15));
       System.out.println("Mi arbolito \n"+miArbolito);
       miArbolito.rotarI(miArbolito.search(miArbolito.raiz,10));
       System.out.println("Mi arbolito \n"+miArbolito);*/
-     
-      
-     /* System.out.println("Mi arbolito \n"+miArbolito);
+
+    /* System.out.println("Mi arbolito \n"+miArbolito);
       miArbolito.add(56);
       System.out.println("Mi arbolito \n"+miArbolito);
       miArbolito.add(60);
@@ -135,7 +159,5 @@ public class Practica4 {
      miArbolito.revisarBalanceInv(miArbolito.search(miArbolito.raiz,89));
      miArbolito.revisarBalanceInv(miArbolito.search(miArbolito.raiz,59));
      System.out.println("Mi arbolito \n"+miArbolito);*/
-        }
-
-
+  }
 }
